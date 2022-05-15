dc7927673fdf91fa13f060f4396c89992a2d9902
idris <idrisbohra786@gmail.com>
2022-05-15 10:56:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import { BrowserRouter ,Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router,  Switch,  Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import RentProperty from './components/RentProperty';
+import Form1 from './components/Form1';
+import Services from './components/Services';
+
+import AddImages from './components/AddImages';
+import Blog from './components/Blog';
+import Home from './components/Home';
+import AddProperty from './components/AddProperty';
+import Type from './components/Type';
+import LoginWithEmail from './components/LoginWithEmail';
+import Register from './components/Register';
+import {UserState} from './Context/UserContext'
+import {AuthState} from './Context/AuthContext'
+ 
+class App extends Component {
+  render() {
+    return (
+    
+<UserState>
+<AuthState>
+      <Router>
+      <Switch>
+      
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path='/AddProperty' >
+        < AddProperty />
+      </Route>
+      <Route exact path='/HostelForm' >
+        < Form1 />
+      </Route>
+      <Route exact path='/RentForm' >
+      < RentProperty />
+      </Route>
+      <Route exact path='/Type' >
+      < Type />
+      </Route>
+      <Route exact path='/AddImages' >
+      < AddImages />
+      </Route>
+      <Route exact path='/Login'>
+      <LoginWithEmail/>
+      </Route>
+      <Route exact path='/Register' >
+      <Register/>
+      </Route>
+      </Switch>
+      </Router>
+      </AuthState>
+      </UserState>
+   );
+  }
 }
-
+ 
 export default App;
